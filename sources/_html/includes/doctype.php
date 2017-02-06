511b8aded50538b428d16eb1554b7137103904c7
Martin Durchholz <mdurchholz@moveo.com>
2017-02-06 13:13:42 -0600

    files updates

--- sources/_html/includes/doctype.php
+++ sources/_html/includes/doctype.php
@@ -1,23 +1,23 @@
-	<meta charset="utf-8" />
-	<link rel="dns-prefetch" href="//ajax.googleapis.com" /><!-- Grabs repo files quicker -->
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><!-- This + conditionals above destroy edge detection issues -->
-	<meta http-equiv="imagetoolbar" content="false" /><!-- Kills IE6 Image toolbar -->
+<meta charset="utf-8" />
+<link rel="dns-prefetch" href="//ajax.googleapis.com" /><!-- Grabs repo files quicker -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><!-- This + conditionals above destroy edge detection issues -->
+<meta http-equiv="imagetoolbar" content="false" /><!-- Kills IE6 Image toolbar -->
 
-	<title></title>
+<title></title>
 
-	<meta name="author" content="" />
-    <meta name="description" content="" />
-	<meta name="keywords" content="" />
-	<meta name="rating" content="general" />
-	<meta name="robots" content="index, follow" />
-	<meta name="content-language" content="english">
-	<meta name="audience" content="all" />
+<meta name="author" content="" />
+<meta name="description" content="" />
+<meta name="keywords" content="" />
+<meta name="rating" content="general" />
+<meta name="robots" content="index, follow" />
+<meta name="content-language" content="english">
+<meta name="audience" content="all" />
 
-	<!-- Disables Scaling on multi-touch interfaces -->
-	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0">
+<!-- Disables Scaling on multi-touch interfaces -->
+<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0">
 
-	<!-- Favicon -->
-	<link type="image/png" rel="icon" href="/assets/img/favicon.png" />
+<!-- Favicon -->
+<link type="image/png" rel="icon" href="/assets/img/favicon.png" />
 
-	<!-- CSS -->
-	<link type="text/css" rel="stylesheet" href="/assets/css/master.min.css" />
+<!-- CSS -->
+<link type="text/css" rel="stylesheet" href="/assets/css/master.min.css" />
